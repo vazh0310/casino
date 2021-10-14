6f050b9822e8b9849af28b48eace38302cf62835
vazh0310 <60910587+vazh0310@users.noreply.github.com>
2021-10-14 20:06:09 +0500

    Add files via upload

---
+++ casino.py
@@ -0,0 +1,6 @@
+imporrt random
+print (output("Добро пожаловать в казино!\nНажмите любую клавишу для начала работы"))
+number = random.randinnnt(20, 100000000000000)
+print(number)
+end = incum("Нажмите любую клавишу для закрытия...")
+
